f764b6b619dc3beceec05523632a90f28ccd7fdd
Marat <m.brezin@yandex.ru>
2025-09-13 20:25:44 +0300

    add 1 task

--- dynamic programming with one parameter/task_1.cpp
+++ dynamic programming with one parameter/task_1.cpp
@@ -2,3 +2,34 @@
 // Created by Marat on 13.09.25.
 //
 
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    if (n == 1) {
+        std::cout << 2;
+        return 0;
+    }
+    if (n == 2) {
+        std::cout << 4;
+        return 0;
+    }
+    if (n == 3) {
+        std::cout << 7;
+        return 0;
+    }
+
+    std::vector<int> dp(n + 1);
+    dp[1] = 2;
+    dp[2] = 4;
+    dp[3] = 7;
+
+    for (int i = 4; i <= n; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+
+    std::cout << dp[n];
+}
