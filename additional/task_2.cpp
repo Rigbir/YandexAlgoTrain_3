4b350cd093e8b2e3d42e72347a5b1a38a2767e66
Marat <m.brezin@yandex.ru>
2025-09-20 02:18:33 +0300

    add 2 task

--- additional/task_2.cpp
+++ additional/task_2.cpp
@@ -1,3 +1,43 @@
 //
 // Created by Marat on 19.09.25.
 //
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    int k;
+    std::cin >> k;
+
+    std::string inputLine;
+    std::cin >> inputLine;
+
+    std::vector<char> letters = {
+        'a','b','c','d','e','f','g','h','i','j','k','l','m',
+        'n','o','p','q','r','s','t','u','v','w','x','y','z'
+    };
+
+    int maxLength = 0;
+    for (int i = 0; i < letters.size(); ++i) {
+        int left = 0;
+        int countChar = 0;
+
+        for (int right = 0; right < inputLine.size(); ++right) {
+            if (inputLine[right] != letters[i]) {
+                ++countChar;
+            }
+
+            while (countChar > k) {
+                if (inputLine[left] != letters[i]) {
+                    --countChar;
+                }
+                ++left;
+            }
+
+            maxLength = std::max(maxLength, right - left + 1);
+        }
+    }
+
+    std::cout << maxLength;
+}
