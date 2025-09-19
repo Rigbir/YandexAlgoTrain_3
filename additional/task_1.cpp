43415423d64d7adf74170a4edc823b6223dd74cc
Marat <m.brezin@yandex.ru>
2025-09-19 22:24:14 +0300

    add 1 task

--- additional/task_1.cpp
+++ additional/task_1.cpp
@@ -4,3 +4,35 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+
+int main() {
+    std::map<char, int> symbols;
+    std::string inputLine;
+
+    while (std::getline(std::cin, inputLine)) {
+        for (auto c : inputLine) {
+            if (c != ' ') {
+                ++symbols[c];
+            }
+        }
+    }
+
+    int maxHeight = 0;
+    for (auto [ch, cnt] : symbols) {
+        if (cnt > maxHeight) {
+            maxHeight = cnt;
+        }
+    }
+
+    for (int i = maxHeight; i > 0; --i) {
+        for (auto [ch, cnt] : symbols) {
+            std::cout << (cnt >= i ? '#' : ' ');
+        }
+        std::cout << '\n';
+    }
+
+    for (auto [ch, cnt] : symbols) {
+        std::cout << ch; 
+    }
+}
