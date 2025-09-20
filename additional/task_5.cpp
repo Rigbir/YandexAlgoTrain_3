6d0148d5077f970af8b5561319c58f29999eeb52
Marat <m.brezin@yandex.ru>
2025-09-20 17:12:42 +0300

    add 5 task

--- additional/task_5.cpp
+++ additional/task_5.cpp
@@ -1,3 +1,23 @@
 //
 // Created by Marat on 19.09.25.
 //
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> count(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> count[i];
+    }
+
+    int minCount = 0;
+    for (int i = 0; i + 1 < n; ++i) {
+        minCount += std::min(count[i], count[i + 1]);
+    }
+
+    std::cout << minCount;
+}
