b0dbca99f937fc9746f388b5b9b5175b18a10ffc
Marat <m.brezin@yandex.ru>
2025-09-21 01:17:07 +0300

    add 8 task

--- additional/task_8.cpp
+++ additional/task_8.cpp
@@ -1,3 +1,36 @@
 //
 // Created by Marat on 19.09.25.
 //
+
+#include <iostream>
+#include <vector>
+#include <climits>
+
+int main() {
+    int k;
+    std::cin >> k;
+
+    std::vector<std::pair<int, int>> v(k);
+    for (int i = 0; i < k; ++i) {
+        std::cin >> v[i].first >> v[i].second;
+    }
+
+    int minX = INT_MAX, maxX = INT_MIN;
+    int minY = INT_MAX, maxY = INT_MIN;
+    for (int i = 0; i < v.size(); ++i) {
+        if (v[i].first < minX) {
+            minX = v[i].first;
+        }
+        if (v[i].first > maxX) {
+            maxX = v[i].first;
+        }
+        if (v[i].second < minY) {
+            minY = v[i].second;
+        }
+        if (v[i].second > maxY) {
+            maxY = v[i].second;
+        }
+    }
+
+    std::cout << minX << ' ' << minY << ' ' << maxX << ' ' << maxY;
+}
