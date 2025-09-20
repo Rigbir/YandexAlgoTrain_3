480cc10e506821a323eb93dd3b42776f7e0bd160
Marat <m.brezin@yandex.ru>
2025-09-20 19:04:24 +0300

    add 5 task

--- additional/task_5.cpp
+++ additional/task_5.cpp
@@ -5,39 +5,19 @@
 #include <iostream>
 #include <vector>
 
-int maxCount(const std::vector<int>& v) {
-    if (v.size() < 2) return 0;
-
-    int minId = 0;
-    int minVal = v[0];
-    for (int i = 1; i < v.size(); ++i) {
-        if (v[i] < minVal) {
-            minVal = v[i];
-            minId = i;
-        }
-    }
-
-    std::vector<int> left(v.begin(), v.begin() + minId);
-    std::vector<int> right(v.begin() + minId + 1, v.end());
-
-    for (auto& x : left) {
-        x -= minVal;
-    }
-    for (auto& x : right) {
-        x -= minVal;
-    }
-
-    return (v.size() - 1) * minVal + maxCount(left) + maxCount(right);
-}
-
 int main() {
     int n;
     std::cin >> n;
 
-    std::vector<int> count(n);
+    std::vector<long long> count(n);
     for (int i = 0; i < n; ++i) {
         std::cin >> count[i];
     }
 
-    std::cout << maxCount(count);
+    long long cnt = 0;
+    for (int i = 0; i + 1 < n; ++i) {
+        cnt += std::min(count[i], count[i + 1]);
+    }
+
+    std::cout << cnt;
 }
