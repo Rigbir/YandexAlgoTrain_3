9b6241d8e67f73e597fd873192de433a6174b9ee
Marat <m.brezin@yandex.ru>
2025-09-21 03:19:58 +0300

    add 10 task

--- additional/task_10.cpp
+++ additional/task_10.cpp
@@ -1,3 +1,20 @@
 //
 // Created by Marat on 19.09.25.
 //
+
+#include <iostream>
+#include <map>
+
+int main() {
+    std::string inputLine;
+    std::cin >> inputLine;
+
+    std::map<char, long long> countSymbols;
+    for (int i = 0; i < inputLine.size(); ++i) {
+        countSymbols[inputLine[i]] += (i + 1) * (inputLine.size() - i);
+    }
+
+    for (const auto [ch, cnt] : countSymbols) {
+        std::cout << ch << ": " << cnt << '\n';
+    }
+}
